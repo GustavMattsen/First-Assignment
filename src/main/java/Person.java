7def1d1ee8f2aed5871e8d5867ecd46a5e0586a6
Gustav Mattsén <mattsen03@gmail.com>
2025-08-09 07:39:26 +0200

    Finished Person.java while considering going back to sleep

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,65 @@
 public class Person {
+    // Variables
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Constructor
+    public Person(int id, String firstName, String lastName, String email) {
+        if (firstName == null || lastName == null || email == null) {
+            throw new IllegalArgumentException("Fields cannot be null");
+        }
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getter = get value, Setter = change value
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName == null) {
+            throw new IllegalArgumentException("First name cannot be null");
+        }
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null) {
+            throw new IllegalArgumentException("Last name cannot be null");
+        }
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email == null) {
+            throw new IllegalArgumentException("Email cannot be null");
+        }
+        this.email = email;
+    }
+
+    // Summary with all relevant info
+    public String getSummary() {
+        return "{id: " + id + ", name: " + firstName + " " + lastName + ", email: " + email + "}";
+    }
 }
