ce15be95a34ddd10eea92c977961c838e03a6782
Gustav Mattsén <mattsen03@gmail.com>
2025-08-11 05:51:51 +0200

    Created the three sequencers and made appropriate comments for clarity

---
+++ src/main/java/Sequencers/PersonSequencer.java
@@ -0,0 +1,22 @@
+package Sequencers;
+
+// this keeps track of what id number to give the next Person
+public class PersonSequencer {
+    // starts at 0 by default
+    private static int currentId;
+
+    // makes a new id by adding 1 to the counter
+    public static int nextId() {
+        return ++currentId;
+    }
+
+    // just see what the counter is at right now
+    public static int getCurrentId() {
+        return currentId;
+    }
+
+    // set the counter manually (if loading from file etc.)
+    public static void setCurrentId(int id) {
+        currentId = id;
+    }
+}
---
+++ src/main/java/Sequencers/TodoItemSequencer.java
@@ -0,0 +1,18 @@
+package Sequencers;
+
+// // this keeps track of what id number to give the next TodoItem
+public class TodoItemSequencer {
+    private static int currentId;
+
+    public static int nextId() {
+        return ++currentId;
+    }
+
+    public static int getCurrentId() {
+        return currentId;
+    }
+
+    public static void setCurrentId(int id) {
+        currentId = id;
+    }
+}
